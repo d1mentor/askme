eddb81fe79b02ba5ff17ab7851f060e389c65066
d1mentor <gerasimenkot92@gmail.com>
2021-07-04 18:35:15 +0300

    questions counter added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+
+  def self.declination(amount, form1, form2, form3)
+    remainder_of_10 = amount % 10
+
+    if (amount % 100).between?(11, 14)
+      "#{amount} #{form3}"
+    elsif remainder_of_10 == 1
+      "#{amount} #{form1}"
+    elsif (remainder_of_10).between?(2, 4)
+      "#{amount} #{form2}"
+    else
+      "#{amount} #{form3}"
+    end
+  end
+
   def index
     # Создаём массив из двух болванок пользователей. Вызываем метод # User.new, который создает модель, не записывая её в базу.
     # У каждого юзера мы прописали id, чтобы сымитировать реальную
@@ -28,8 +43,13 @@ class UsersController < ApplicationController
     avtar_url: 'https://static10.tgstat.ru/channels/_0/15/15e0f5c67b87b751e377b4dbb3c1dc74.jpg' )
 
     @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016'))
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
     ]
+
+    @questions_count = UsersController.declination(@questions.size, 'Вопрос', 'Вопроса', 'Вопросов')
 
     @new_question = Question.new
   end
