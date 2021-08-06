3ecbf1e3f2bd598b4ee16c3915c6915fce63dda8
d1mentor <gerasimenkot92@gmail.com>
2021-08-06 17:02:47 +0300

    add declinaton helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,20 +17,6 @@ class UsersController < ApplicationController
   # тех, у кого вообще нет аккаунта на нашем сайте.
   before_action :authorize_user, except: [:index, :new, :create, :show]
 
-  def self.declination(amount, form1, form2, form3)
-    remainder_of_10 = amount % 10
-
-    if (amount % 100).between?(11, 14)
-      "#{amount} #{form3}"
-    elsif remainder_of_10 == 1
-      "#{amount} #{form1}"
-    elsif (remainder_of_10).between?(2, 4)
-      "#{amount} #{form2}"
-    else
-      "#{amount} #{form3}"
-    end
-  end
-
   def index
     @users = User.all
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+  def declination(amount, form1, form2, form3)
+    remainder_of_10 = amount % 10
+
+    if (amount % 100).between?(11, 14)
+      "#{amount} #{form3}"
+    elsif remainder_of_10 == 1
+      "#{amount} #{form1}"
+    elsif (remainder_of_10).between?(2, 4)
+      "#{amount} #{form2}"
+    else
+      "#{amount} #{form3}"
+    end
+  end
 end
